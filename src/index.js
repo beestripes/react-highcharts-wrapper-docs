475c7be76212f7e88373e3a56ae505afb0be3df4
Elise Chant <elise.chant@gmail.com>
2017-09-01 18:46:58 +1000

    remove sw (#9)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 
 require('prismjs');
 require('prismjs/themes/prism.css');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
